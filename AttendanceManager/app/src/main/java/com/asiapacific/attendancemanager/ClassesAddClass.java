bb2d081ff63cdbb1e20756fb3917792bb198f2a9
kmbarrion <barrion.kevin@gmail.com>
2018-07-16 21:24:35 +0800

    imports for adding class java class

---
+++ AttendanceManager/app/src/main/java/com/asiapacific/attendancemanager/ClassesAddClass.java
@@ -0,0 +1,33 @@
+package com.asiapacific.attendancemanager;
+
+import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
+import android.app.TimePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class ClassesAddClass extends AppCompatActivity {
+
+  
+    }
+}
