1366f105b3675c768b3a1623c74c79dddc91589d
chantal15 <chantalsaldivar530@gmail.com>
2018-07-16 20:56:42 +0800

    Setting bottom navigation menu with fragments

---
+++ AttendanceManager/app/src/main/java/com/asiapacific/attendancemanager/ProfMainActivity.java
@@ -0,0 +1,38 @@
+package com.asiapacific.attendancemanager;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class ProfMainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prof_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+        BottomNavigationViewHelper.removeShiftMode(navigation);
+
+        loadFragment(new FragmentProfClasses());
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if(fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+   
+    }
+}
