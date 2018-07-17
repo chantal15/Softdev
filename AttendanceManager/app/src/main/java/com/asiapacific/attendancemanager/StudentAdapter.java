a75efee07d90233d6b03a7a5614887b66a6c465a
kmbarrion <barrion.kevin@gmail.com>
2018-07-17 23:03:08 +0800

    for setting up student adapter for student layout's recycler vieww

---
+++ AttendanceManager/app/src/main/java/com/asiapacific/attendancemanager/StudentAdapter.java
@@ -0,0 +1,32 @@
+package com.asiapacific.attendancemanager;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Setting up StudentAdapter for Recycler View.
+ */
+
+public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.StudentViewHolder> {
+
+    private Context context;
+    private List<Student> studentList;
+
+    public StudentAdapter(Context context, List<Student> studentList) {
+        this.context = context;
+        this.studentList = studentList;
+    }
+
+
+        }
+    }
+}
