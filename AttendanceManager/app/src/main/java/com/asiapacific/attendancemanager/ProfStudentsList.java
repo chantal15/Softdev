e08508a8efde1ae987b31ff77aa46168b22f2d2f
chantal15 <chantalsaldivar530@gmail.com>
2018-08-06 17:23:49 +0800

    Modify ProfStudentsList Java class - added imports

--- AttendanceManager/app/src/main/java/com/asiapacific/attendancemanager/ProfStudentsList.java
+++ AttendanceManager/app/src/main/java/com/asiapacific/attendancemanager/ProfStudentsList.java
@@ -1,16 +1,22 @@
 package com.asiapacific.attendancemanager;
 
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
