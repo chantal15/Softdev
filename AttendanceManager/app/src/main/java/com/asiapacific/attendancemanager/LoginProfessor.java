1b92929ba502b43c0fada0152f7e8e0e9b5d23de
chantal15 <chantalsaldivar530@gmail.com>
2018-08-06 14:21:46 +0800

    fix LoginProfessor java class - added import for google sign-in

--- AttendanceManager/app/src/main/java/com/asiapacific/attendancemanager/LoginProfessor.java
+++ AttendanceManager/app/src/main/java/com/asiapacific/attendancemanager/LoginProfessor.java
@@ -2,11 +2,21 @@ package com.asiapacific.attendancemanager;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class LoginProfessor extends AppCompatActivity {
 
