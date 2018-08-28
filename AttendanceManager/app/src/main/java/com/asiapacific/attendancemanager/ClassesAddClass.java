c870abd664e174b684958f9c5b5ecda786b58db4
alyssaanne <jayalyfantonalgo@gmail.com>
2018-08-28 23:00:47 +0800

    Added array adapter and spinner in the import list

--- AttendanceManager/app/src/main/java/com/asiapacific/attendancemanager/ClassesAddClass.java
+++ AttendanceManager/app/src/main/java/com/asiapacific/attendancemanager/ClassesAddClass.java
@@ -11,12 +11,14 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Spinner;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
